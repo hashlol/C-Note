9d2ef5d43e5ade74ccb876663d39929a0b489520
Nathanvstheworld <castillo.nathan789@gmail.com>
2024-06-19 23:34:01 -0400

    - Added research-options.js in order to search selected text in FIU library and define words in Dictionary - Modified Manifest.json to update dependancies - Modified service-worker-utils.js to change name

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
 console.log("This prints to the console of the service worker (background script)")
 
 // Importing and using functionality from external files is also possible.
-importScripts('service-worker-utils.js')
+importScripts('research-options.js')
